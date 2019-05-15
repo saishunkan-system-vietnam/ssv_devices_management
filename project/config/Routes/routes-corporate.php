39f9ac2863e9fa7f36c17078237f4382ff643e28
HoangND <hoangnguyenit98@gmail.com>
2019-05-15 13:53:45 +0700

    update routes

--- project/config/Routes/routes-corporate.php
+++ project/config/Routes/routes-corporate.php
@@ -63,8 +63,12 @@ Router::scope('/api/v1/', ['prefix' => 'Api'], function (RouteBuilder $routes) {
     $routes->connect('user/update', ['controller' => 'Users', 'action' => 'edit', 'allowWithoutToken' => true]);
 
     // $routes->connect('/schedules/', ['controller' => 'Admin/Schedules', 'action' => 'index']);
+    
+    //Borrow
     $routes->connect('borrow', ['controller' => 'Borrow', 'action' => 'index', 'allowWithoutToken' => true]);
-    $routes->connect('borrow/view/*', ['controller' => 'Borrow', 'action' => 'view', 'allowWithoutToken' => true]);
-    $routes->connect('borrow/add/*', ['controller' => 'Borrow', 'action' => 'add', 'allowWithoutToken' => true]);
+    $routes->connect('borrow/view/:id', ['controller' => 'Borrow', 'action' => 'view', 'allowWithoutToken' => true], ['id' => '\d+', 'pass' => ['id']]);
+    $routes->connect('borrow/add', ['controller' => 'Borrow', 'action' => 'add', 'allowWithoutToken' => true]);
+    $routes->connect('borrow/edit/:id', ['controller' => 'Borrow', 'action' => 'editBorrowDevices', 'allowWithoutToken' => true], ['id' => '\d+', 'pass' => ['id']]);
+    $routes->connect('borrow/delete/:id', ['controller' => 'Borrow', 'action' => 'delete', 'allowWithoutToken' => true], ['id' => '\d+', 'pass' => ['id']]);
     
 });
