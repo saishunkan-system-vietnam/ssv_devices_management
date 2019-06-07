bf97c60bf59b3b6f3a9543d046fb403bfe6a50a0
HungHT <hunght@saisystem.vn>
2019-06-07 10:29:16 +0700

    - fix page

--- multi-page/src/components/Menu.js
+++ multi-page/src/components/Menu.js
@@ -6,11 +6,7 @@ function Menu() {
     return (
       <ul className={style.Menu}>
         <li><a href="/index.html">Home</a></li>
-        <li><a href="/products/product-1.html">Product</a></li>
-        <li><a href="/contacts/contact.html">Contact</a></li>
         <li><a href="/signin/signin.html">Signin</a></li>
-        <li><a href="/users/update.html">Users</a></li>
-        <li><a href="/login/login.html">Login</a></li>
       </ul>
     );
 }
